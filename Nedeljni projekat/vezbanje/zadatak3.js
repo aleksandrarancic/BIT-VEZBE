cd282d283373ce72960b258eb6f8b8d2d553deab
aleksandra rancic <aleksandra.rancic82@gmail.com>
2022-10-26 19:46:47 +0200

    zadatak3

---
+++ Nedeljni projekat/vezbanje/zadatak3.js
@@ -0,0 +1,9 @@
+var umnozak=1;
+var sum=0;
+var a =[1, 5, -7, 10, 17, -1];
+for(var i=0; i<a.length; i++){
+    sum=sum+a[i];
+    umnozak=umnozak * a[i];
+}
+console.log(sum);
+console.log(umnozak);
