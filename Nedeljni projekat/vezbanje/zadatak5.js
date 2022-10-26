ea502f1ce68d8b509d806690567cd8c4c87e483b
aleksandra rancic <aleksandra.rancic82@gmail.com>
2022-10-26 20:26:04 +0200

    zadatak5

---
+++ Nedeljni projekat/vezbanje/zadatak5.js
@@ -0,0 +1,12 @@
+
+var a = [
+    [1, 2, 1, 24],
+    [8, 11, 9, 4],
+    [7, 0, 7, 27]
+];
+for(var i=0; i<a.length; i++){
+    for(var j=0; j<a[i].length; j++){
+        console.log(a[i][j]);
+    }
+}
+    
